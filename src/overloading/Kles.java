49816f3383a62a4293412c12911f60a4ce3b716a
25-Naufal <naufal_rafif_29rpl@student.smktelkom-mlg.sch.id>
2021-03-30 18:41:39 +0700

    hore berhasil

---
+++ src/overloading/Kles.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overloading;
+
+/**
+ *
+ * @author ASUS X450JN
+ */
+public class Kles {
+    public void hitung (int a, int b){
+        System.out.println("Luas persegi adalah = "+(a*b));
+        //2 variabel 
+    }
+    public void hitung (int a, int b, int c){
+        System.out.println("Volume balok adalah = "+(a*b*c));
+        //3 variabel
+        
+    }
+    
+}
---
+++ src/overloading/Overloading.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overloading;
+
+/**
+ *
+ * @author ASUS X450JN
+ */
+public class Overloading {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kles object = new Kles();
+        //memanggil kles
+        object.hitung(4, 4);
+        object.hitung(4, 5, 6);
+        //memanggil hitung ke dua duanya
+    }
+    
+}
